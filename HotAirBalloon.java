45bc8f939a13ea1a40eaf3f8e55519c74235a474
Sabirah Shuaybi <sabirah.shuaybi@gmail.com>
2016-11-24 20:39:45 -0500

    Update HotAirBalloon.java

--- HotAirBalloon.java
+++ HotAirBalloon.java
@@ -26,72 +26,73 @@ public class HotAirBalloon
     private static final int SANDBAG_WIDTH = 2;
     private static final int SANDBAG_HEIGHT = 10;
 
-    //Constructor for HotAirBalloon class
+    /* Constructor for HotAirBalloon class */
     public HotAirBalloon (DrawingCanvas canvas, Location location) {
 
-       //Dynamically set x and y coordinates of basket location
-       double basketX = location.getX() - BASKET_SIZE/2;
-       double basketY = location.getY() + BASKET_DISTANCE;
+        //Dynamically set x and y coordinates of basket location
+        double basketX = location.getX() - BASKET_SIZE/2;
+        double basketY = location.getY() + BASKET_DISTANCE;
 
-       basket = new FilledRect(basketX, basketY, BASKET_SIZE, BASKET_SIZE, canvas);
+        basket = new FilledRect(basketX, basketY, BASKET_SIZE, BASKET_SIZE, canvas);
 
-       //Set x and y coordinates of left most sandbag relative to basket
-       double leftSandbagX = basketX + BASKET_SIZE/4;
-       double leftSandbagY = basketY;
+        //Set x and y coordinates of left most sandbag relative to basket
+        double leftSandbagX = basketX + BASKET_SIZE/4;
+        double leftSandbagY = basketY;
 
-       leftSandbag = new FilledRect(leftSandbagX, leftSandbagY, SANDBAG_WIDTH, SANDBAG_HEIGHT, canvas);
-       //Set color of sandbag to yellow
-       leftSandbag.setColor(new Color(255, 255, 0));
+        leftSandbag = new FilledRect(leftSandbagX, leftSandbagY, SANDBAG_WIDTH, SANDBAG_HEIGHT, canvas);
+        //Set color of sandbag to yellow
+        leftSandbag.setColor(new Color(255, 255, 0));
 
-       //Set x and y coordinates of right sandbag relative to basket
-       double rightSandbagX = basketX + BASKET_SIZE - 7;
-       double rightSandbagY = basketY;
+        //Set x and y coordinates of right sandbag relative to basket
+        double rightSandbagX = basketX + BASKET_SIZE - 7;
+        double rightSandbagY = basketY;
 
-       rightSandbag = new FilledRect(rightSandbagX, rightSandbagY, SANDBAG_WIDTH, SANDBAG_HEIGHT, canvas);
-       //Set color of sandbag to yellow
-       rightSandbag.setColor(new Color(255, 255, 0));
+        rightSandbag = new FilledRect(rightSandbagX, rightSandbagY, SANDBAG_WIDTH, SANDBAG_HEIGHT, canvas);
+        //Set color of sandbag to yellow
+        rightSandbag.setColor(new Color(255, 255, 0));
 
-       //Set x and y coordinates for left rope start point
-       double leftStartX = location.getX() - BALLOON_SIZE/2;
-       double leftStartY = location.getY();
-       //Set x and y coordinates for left rope end point
-       double leftEndX = basketX;
-       double leftEndY = basketY;
+        //Set x and y coordinates for left rope start point
+        double leftStartX = location.getX() - BALLOON_SIZE/2;
+        double leftStartY = location.getY();
+        //Set x and y coordinates for left rope end point
+        double leftEndX = basketX;
+        double leftEndY = basketY;
 
-       leftRope = new Line(leftStartX, leftStartY, leftEndX, leftEndY, canvas);
+        leftRope = new Line(leftStartX, leftStartY, leftEndX, leftEndY, canvas);
 
-       //Set x and y coordinates for right rope start point
-       double rightStartX = location.getX() + BALLOON_SIZE/2;
-       double rightStartY = location.getY();
-       //Set x and y coordinates for right rope end point
-       double rightEndX = basketX + BASKET_SIZE;
-       double rightEndY = basketY;
+        //Set x and y coordinates for right rope start point
+        double rightStartX = location.getX() + BALLOON_SIZE/2;
+        double rightStartY = location.getY();
+        //Set x and y coordinates for right rope end point
+        double rightEndX = basketX + BASKET_SIZE;
+        double rightEndY = basketY;
 
-       rightRope =  new Line(rightStartX, rightStartY, rightEndX, rightEndY, canvas);
+        rightRope =  new Line(rightStartX, rightStartY, rightEndX, rightEndY, canvas);
 
-       //Dynamically set x and y coordinates of balloon (determined by user mouse click)
-       double balloonX = (location.getX() - (BALLOON_SIZE/2));
-       double balloonY = (location.getY() - (BALLOON_SIZE/2));
-       //Constructor for balloon
-       balloon = new FilledOval(balloonX, balloonY, BALLOON_SIZE, BALLOON_SIZE, canvas);
-       balloon.setColor(randomColor());
+        //Dynamically set x and y coordinates of balloon (determined by user mouse click)
+        double balloonX = (location.getX() - (BALLOON_SIZE/2));
+        double balloonY = (location.getY() - (BALLOON_SIZE/2));
+        //Constructor for balloon
+        balloon = new FilledOval(balloonX, balloonY, BALLOON_SIZE, BALLOON_SIZE, canvas);
+        balloon.setColor(randomColor());
 
-       //Dynamically set x and y coordinates for MHC banner
-       double MHCX = location.getX() - BALLOON_SIZE/2 + 8;
-       double MHCY = location.getY() - 13;
-       MHC = new Text("MHC", MHCX, MHCY, canvas);
+        //Dynamically set x and y coordinates for MHC banner
+        double MHCX = location.getX() - BALLOON_SIZE/2 + 8;
+        double MHCY = location.getY() - 13;
+        MHC = new Text("MHC", MHCX, MHCY, canvas);
 
-       //Set MHC text color to white
-       MHC.setColor(new Color(255, 255, 255));
-       //Get font size to print on terminal in order to then adjust font size
-       System.out.println("MHC Font Size" + MHC.getFont());
-       //Set MHC text font size to 23 (was originally 13)
-       int fontSize = 23;
-       MHC.setFontSize(fontSize);
+        //Set MHC text color to white
+        MHC.setColor(new Color(255, 255, 255));
+        //Get font size to print on terminal in order to then adjust font size
+        System.out.println("MHC Font Size" + MHC.getFont());
+        //Set MHC text font size to 23 (was originally 13)
+        int fontSize = 23;
+        MHC.setFontSize(fontSize);
     }
 
     /* Determines if mouse click is contained within any part of hot air balloon */
     public boolean contains(Location mouseClick) {
+
         if (balloon.contains(mouseClick))
             return true;
         if (basket.contains(mouseClick))
@@ -100,20 +101,22 @@ public class HotAirBalloon
             return true;
         if (rightRope.contains(mouseClick))
             return true;
-       //If none of the above conditions are met, return the boolean flag false (need to know this info for the onMouseDrag method in BalloonController Class)
+
         return false;
 
     }
 
     /* Creates and returns a purely random color */
     public Color randomColor() {
-      //Invoking the Random() method and assigning it to variable r of type Random
-       Random r = new Random();
-       int red = r.nextInt(255);
-       int green = r.nextInt(255);
-       int blue = r.nextInt(255);
-       //Setting the balloon to a random color through invokation of Random() method and assigning the result of this method to three variables (red, green, blue)
-       return new Color(red, green, blue);
+
+        //Randomly assign value ranging from 0-255 to each RGB component 
+        Random r = new Random();
+        int red = r.nextInt(255);
+        int green = r.nextInt(255);
+        int blue = r.nextInt(255);
+        
+        //Set balloon to a random color 
+        return new Color(red, green, blue);
     }
 
     /* Moves entire hot air balloon up and down */
@@ -141,6 +144,5 @@ public class HotAirBalloon
 
         balloon.setColor(balloon.getColor().brighter());
     }
-
 
 }
